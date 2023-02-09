173b3080827482a7b51ff4b6f32bc0461f6a789e
jatoad <jasperhmca.todd@gmail.com>
2023-02-09 14:54:45 +0000

    beginning to ad in questions, creating Javascript function for submitting answer

---
+++ assets/js/script.js
@@ -0,0 +1,34 @@
+// code from love maths walkthrough project
+document.addEventListener("DOMContentLoaded", function(){
+    let buttons = document.getElementsByTagName("button");
+})
+
+// code from love maths walkthrough project
+
+function submitAnswer(){
+    let submitButton = document.getElementsByClassName('submit-button');
+    submitButton.addEventListener('',function(){
+
+    })
+
+}
+
+function checkAnswer(){
+    let checkAnswer1 = document.getElementById('correct1').innerHTML;
+    let checkAnswer2 = document.getElementById('correct2').innerHTML;
+    let checkAnswer3 = document.getElementById('correct3').innerHTML;
+    let checkAnswer4 = document.getElementById('correct4').innerHTML;
+    let checkAnswer5 = document.getElementById('correct5').innerHTML;
+    let checkAnswer6 = document.getElementById('correct6').innerHTML;
+    let checkAnswer7 = document.getElementById('correct7').innerHTML;
+    let checkAnswer8 = document.getElementById('correct8').innerHTML;
+    let checkAnswer9 = document.getElementById('correct9').innerHTML;
+    let checkAnswer10 = document.getElementById('correct10').innerHTML;
+    
+
+
+}
+
+function addUsername(){
+
+}
